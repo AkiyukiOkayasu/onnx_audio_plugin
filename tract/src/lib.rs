2a7b420f2be44c4d34f7b90e49d1cea8bcfcb0a8
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2024-12-18 16:56:20 +0900

    fix: onnx path

--- tract/src/lib.rs
+++ tract/src/lib.rs
@@ -14,7 +14,7 @@ struct OnnxAudioPluginParams {}
 
 impl Default for OnnxAudioPlugin {
     fn default() -> Self {
-        let onnx_model = include_bytes!("../../onnx/linear/linear.onnx");
+        let onnx_model = include_bytes!("../../onnx/linear.onnx");
 
         let model = onnx()
             // load the model
